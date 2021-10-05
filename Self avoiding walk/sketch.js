82af7d82a819454243ab71a4ff1edf7fb49070f2
younissk <younisskandah@gmail.com>
2021-10-05 18:03:32 +0200

    self avoiding walk with backtrack

--- Self avoiding walk/sketch.js
+++ Self avoiding walk/sketch.js
@@ -1,59 +1,82 @@
-let walker;
-
-let x;
-let y;
-
-let allOptions = [
-  { dx: 1, dy: 0 },
-  { dx: -1, dy: 0 },
-  { dx: 0, dy: 1 },
-  { dx: 0, dy: -1 },
-];
+// Made with the help of the coding train tutorial
 
 let grid;
-let spacing = 100;
-let cols;
-let rows;
+let spacing = 80;
+let cols, rows;
+let path = [];
+let spot;
+
+let offset = 0;
 
 function make2DArray(cols, rows) {
   let arr = new Array(cols);
-  for (let i = 0; i < arr.length; i++) {
+  for (var i = 0; i < arr.length; i++) {
     arr[i] = new Array(rows);
   }
   return arr;
 }
 
-function setupSketch() {
+function setup() {
+  createCanvas(windowWidth, windowHeight);
   cols = floor(width / spacing);
   rows = floor(height / spacing);
-  x = floor(cols / 2);
-  y = floor(rows / 2);
   background(51);
   grid = make2DArray(cols, rows);
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
-      grid[i][j] = false;
+      grid[i][j] = new Spot(i, j);
     }
   }
-  console.log(x, y);
-  walker = new Walker(x, y);
-  grid[x][y] = true;
-  frameRate(10);
-}
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  setupSketch();
-}
-
-function draw() {
-  walker.show();
-  walker.update();
+  let startx = floor(cols / 2);
+  let starty = floor(rows / 2);
+  spot = grid[startx][starty];
+  path.push(spot);
+  spot.visited = true;
 }
 
 function isValid(i, j) {
   if (i < 0 || i >= cols || j < 0 || j >= rows) {
     return false;
   }
-  return !grid[i][j];
+  return !grid[i][j].visited;
+}
+
+function draw() {
+  frameRate(20);
+
+  let r = map(sin(offset), 0, 1, 0, 255);
+  let g = map(cos(offset), 0, 1, 0, 255);
+  let b = map(noise(offset), 0, 1, 0, 255);
+
+  background(map(sin(offset), 0, 1, 10, 51));
+  translate(spacing * 0.5, spacing * 0.5);
+
+  spot = spot.nextSpot();
+  if (!spot) {
+    let stuck = path.pop();
+    stuck.clear();
+    spot = path[path.length - 1];
+  } else {
+    path.push(spot);
+    spot.visited = true;
+  }
+
+  if (path.length === cols * rows) {
+    console.log("Solved!");
+    noLoop();
+  }
+
+  stroke(r, g, b);
+  strokeWeight(spacing * 0.1);
+  noFill();
+  beginShape();
+  for (let spot of path) {
+    vertex(spot.x, spot.y);
+  }
+  endShape();
+
+  stroke(r, g, b);
+  strokeWeight(spacing * 0.5);
+  point(spot.x, spot.y);
+  offset += 0.01;
 }
